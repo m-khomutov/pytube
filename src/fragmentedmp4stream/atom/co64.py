1f77ad25e2995338bbb9d624d5e448962bcdcb1a
m.homutov <m.homutov@netris.ru>
2021-08-19 14:13:13 +0300

    refactored co64 box

--- src/fragmentedmp4stream/atom/co64.py
+++ src/fragmentedmp4stream/atom/co64.py
@@ -1,32 +1,30 @@
+"""The chunk offset table gives the index of each chunk into the containing file.
+   The variant, permitting the use of 64-bit offsets
+"""
+from functools import reduce
 from .atom import FullBox
 
 
 class Box(FullBox):
+    """64-bit chunk offset box"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        f = kwargs.get("file", None)
+        file = kwargs.get("file", None)
         self.entries = []
-        if f != None:
-            self._readfile(f)
+        if file is not None:
+            self._readfile(file)
         else:
             self.type = 'co64'
             self.size = 16
 
     def __repr__(self):
-        ret = super().__repr__() + " offsets:[ "
-        for off in self.entries:
-            ret += str(off) + ' '
-        ret += "]"
-        return ret
+        return super().__repr__() + ' offsets:[' + ' '.join([str(k) for k in self.entries]) + ']'
 
-    def _readfile(self, f):
-        count = int.from_bytes(self._readsome(f, 4), "big")
-        for i in range(count):
-            self.entries.append(int.from_bytes(self._readsome(f, 8), "big"))
+    def _readfile(self, file):
+        count = int.from_bytes(self._readsome(file, 4), "big")
+        self.entries = [map(lambda: int.from_bytes(self._readsome(file, 8), 'big'), range(count))]
 
-    def encode(self):
-        ret = super().encode()
-        ret += len(self.entries).to_bytes(4, byteorder='big')
-        for off in self.entries:
-            ret += off.to_bytes(8, byteorder='big')
-        return ret
+    def to_bytes(self):
+        ret = super().to_bytes() + len(self.entries).to_bytes(4, byteorder='big')
+        return ret + reduce(lambda a, b: a + b,
+                            map(lambda x: x.to_bytes(8, byteorder='big'), self.entries))
