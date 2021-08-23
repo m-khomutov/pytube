df6a1d13873634f2a0a8ad1fcdc3d43e08eb0617
m.homutov <m.homutov@netris.ru>
2021-08-23 13:05:27 +0300

    refactored hdlr box

--- src/fragmentedmp4stream/atom/hdlr.py
+++ src/fragmentedmp4stream/atom/hdlr.py
@@ -1,32 +1,34 @@
+"""Declares the process by which the media-data in the track is presented,
+   and thus, the nature of the media in a track.
+"""
 from .atom import FullBox
 
 
 class Box(FullBox):
+    """The media handler type box"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        f = kwargs.get("file", None)
-        if f != None:
-            self._readfile(f)
+        file = kwargs.get("file", None)
+        if file is not None:
+            self._readfile(file)
 
     def __repr__(self):
-        ret = super().__repr__() + " handlerType:" + self.handler_type + \
-              " name:" + self.name
-        return ret
+        return super().__repr__() + f" handler type:{self.handler_type} name:{self.name}"
 
-    def _readfile(self, f):
-        self._readsome(f, 4)
-        self.handler_type = self._readsome(f, 4).decode("utf-8")
-        self._readsome(f, 12)
-        left = (self.size - (f.tell() - self.position))
+    def _readfile(self, file):
+        self._readsome(file, 4)
+        self.handler_type = self._readsome(file, 4).decode("utf-8")
+        self._readsome(file, 12)
+        left = (self.size - (file.tell() - self.position))
         if left > 0:
-            self.name = self._readsome(f, left).decode("utf-8")
+            self.name = self._readsome(file, left).decode("utf-8")
 
-    def encode(self):
-        ret = super().encode()
-        ret += (0).to_bytes(4, byteorder='big')
-        ret += str.encode(self.handler_type)
-        ret += (0).to_bytes(4, byteorder='big')
-        ret += (0).to_bytes(8, byteorder='big')
+    def to_bytes(self):
+        ret = super().to_bytes() +\
+              (0).to_bytes(4, byteorder='big') + \
+              str.encode(self.handler_type) + \
+              (0).to_bytes(4, byteorder='big') + \
+              (0).to_bytes(8, byteorder='big')
         if len(self.name) > 0:
             ret += str.encode(self.name)
         return ret
