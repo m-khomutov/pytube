f55697e0df8283a51dd60a06af6d046ebb5d8789
m.homutov <m.homutov@netris.ru>
2021-08-24 13:36:20 +0300

    refactored mdhd box

--- src/fragmentedmp4stream/atom/mdhd.py
+++ src/fragmentedmp4stream/atom/mdhd.py
@@ -1,60 +1,60 @@
-from .atom import FullBox
+"""The media header declares overall information that is media-independent,
+   and relevant to characteristics of the media in a track.
+"""
 from bitstring import BitArray
+from .atom import FullBox
+
 
 class Box(FullBox):
+    """Media header box, overall information about the media"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        f = kwargs.get("file", None)
-        if f != None:
-            self._readfile(f)
+        file = kwargs.get("file", None)
+        if file is not None:
+            self._readfile(file)
 
     def __repr__(self):
-        ret = super().__repr__() + " ctime:" + str(self.creationTime) + \
-              " mtime:" + str(self.modificationTime) + \
-              " timescale:" + str(self.timescale) + \
-              " duration:" + str(self.duration) + \
-              " language:" + str(self.language)
-        return ret
+        return super().__repr__() + \
+              f' creation time:{self.creation_time}' \
+              f' modification time:{self.modification_time}' \
+              f' timescale:{self.timescale}' \
+              f' duration:{self.duration}' \
+              f' language:{self.language}'
 
-    def _readfile(self, f):
+    def _readfile(self, file):
         if self.version == 1:
-            self.creationTime = int.from_bytes(self._readsome(f, 8), "big")
-            self.modificationTime = int.from_bytes(self._readsome(f, 8), "big")
-            self.timescale = int.from_bytes(self._readsome(f, 4), "big")
-            self.duration = int.from_bytes(self._readsome(f, 8), "big")
+            self.creation_time = int.from_bytes(self._readsome(file, 8), "big")
+            self.modification_time = int.from_bytes(self._readsome(file, 8), "big")
+            self.timescale = int.from_bytes(self._readsome(file, 4), "big")
+            self.duration = int.from_bytes(self._readsome(file, 8), "big")
         else:
-            self.creationTime = int.from_bytes(self._readsome(f, 4), "big")
-            self.modificationTime = int.from_bytes(self._readsome(f, 4), "big")
-            self.timescale = int.from_bytes(self._readsome(f, 4), "big")
-            self.duration = int.from_bytes(self._readsome(f, 4), "big")
+            self.creation_time = int.from_bytes(self._readsome(file, 4), "big")
+            self.modification_time = int.from_bytes(self._readsome(file, 4), "big")
+            self.timescale = int.from_bytes(self._readsome(file, 4), "big")
+            self.duration = int.from_bytes(self._readsome(file, 4), "big")
 
-        l = BitArray(self._readsome(f, 2))
-        self.language = ""
-        for i in range(3):
-            b=5*i+1
-            e=b+5
-            self.language += chr(int(l[b:e].bin, 2)+0x60)
-        self._readsome(f, 2)
+        lang = BitArray(self._readsome(file, 2))
+        self.language = ''.join(chr(k) for k in
+                                map(lambda x: int(lang[x+1:x+6].bin, 2)+0x60, range(0, 15, 5)))
+        self._readsome(file, 2)
 
-    def encode(self):
-        ret = super().encode()
+    def to_bytes(self):
+        ret = super().to_bytes()
         if self.version == 1:
-            ret += self.creationTime.to_bytes(8, byteorder='big')
-            ret += self.modificationTime.to_bytes(8, byteorder='big')
+            ret += self.creation_time.to_bytes(8, byteorder='big')
+            ret += self.modification_time.to_bytes(8, byteorder='big')
             ret += self.timescale.to_bytes(4, byteorder='big')
             ret += self.duration.to_bytes(8, byteorder='big')
         else:
-            ret += self.creationTime.to_bytes(4, byteorder='big')
-            ret += self.modificationTime.to_bytes(4, byteorder='big')
+            ret += self.creation_time.to_bytes(4, byteorder='big')
+            ret += self.modification_time.to_bytes(4, byteorder='big')
             ret += self.timescale.to_bytes(4, byteorder='big')
             ret += self.duration.to_bytes(4, byteorder='big')
-
-        lang = 0
-        offset=14
-        for c in self.language:
-            d = ord(c)-0x60
+        lang, offset = 0, 14
+        for symbol in self.language:
+            digit = ord(symbol)-0x60
             for i in range(5):
-                lang |= ((d >> i) & 1)<<offset
+                lang |= ((digit >> i) & 1) << offset
                 offset -= 1
         ret += lang.to_bytes(2, byteorder='big')
         ret += (0).to_bytes(2, byteorder='big')
