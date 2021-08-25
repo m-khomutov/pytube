23cfcfefb0edd54856f793554c34492da999999d
m.homutov <m.homutov@netris.ru>
2021-08-25 13:25:59 +0300

    refactored mvhd box

--- src/fragmentedmp4stream/atom/mvhd.py
+++ src/fragmentedmp4stream/atom/mvhd.py
@@ -1,57 +1,60 @@
+"""Defines overall information which is media-independent,
+   and relevant to the entire presentation considered as a whole
+"""
+from functools import reduce
 from .atom import FullBox
 
 
 class Box(FullBox):
+    """Movie header, overall declarations"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        f = kwargs.get("file", None)
-        if f != None:
-            self._readfile(f)
+        file = kwargs.get("file", None)
+        if file is not None:
+            self._readfile(file)
 
     def __repr__(self):
-        ret = super().__repr__() + " ctime:" + str(self.creationTime) + \
-                                   " mtime:" + str(self.modificationTime) + \
-                                   " timescale:" + str(self.timescale) + \
-                                   " duration:" + str(self.duration) + \
-                                   " rate:" + hex(self.rate) + \
-                                   " volume:" + hex(self.volume) + \
-                                   " matrix:[ "
-        for i in self.matrix:
-            ret += hex(i) + " "
-        ret += "] nextTrackID:" + str(self.nextTrackID)
+        ret = super().__repr__() + \
+              f" creation time:{self.time[0]} " \
+              f"modification time:{self.time[1]} " \
+              f"timescale:{self.timescale} " \
+              f"duration:{self.duration} " \
+              f"rate:{self.rate} volume:{self.volume} "
+        ret += 'matrix:[' + ' '.join(hex(k) for k in self.matrix) + ']'
+        ret += f" nextTrackID:{self.next_track_id}"
         return ret
 
-    def _readfile(self, f):
+    def _readfile(self, file):
         if self.version == 1:
-            self.creationTime = int.from_bytes(self._readsome(f, 8), "big")
-            self.modificationTime = int.from_bytes(self._readsome(f, 8), "big")
-            self.timescale = int.from_bytes(self._readsome(f, 4), "big")
-            self.duration = int.from_bytes(self._readsome(f, 8), "big")
+            self.time = (int.from_bytes(self._readsome(file, 8), "big"),
+                         int.from_bytes(self._readsome(file, 8), "big"))
+            self.timescale = int.from_bytes(self._readsome(file, 4), "big")
+            self.duration = int.from_bytes(self._readsome(file, 8), "big")
         else:
-            self.creationTime = int.from_bytes(self._readsome(f, 4), "big")
-            self.modificationTime = int.from_bytes(self._readsome(f, 4), "big")
-            self.timescale = int.from_bytes(self._readsome(f, 4), "big")
-            self.duration = int.from_bytes(self._readsome(f, 4), "big")
+            self.time = (int.from_bytes(self._readsome(file, 4), "big"),
+                         int.from_bytes(self._readsome(file, 4), "big"))
+            self.timescale = int.from_bytes(self._readsome(file, 4), "big")
+            self.duration = int.from_bytes(self._readsome(file, 4), "big")
 
-        self.rate = int.from_bytes(self._readsome(f, 4), "big")
-        self.volume = int.from_bytes(self._readsome(f, 2), "big")
-        self._readsome(f, 10)
-        self.matrix = []
-        for i in range(9):
-            self.matrix.append(int.from_bytes(self._readsome(f, 4), "big"))
-        self._readsome(f, 24)
-        self.nextTrackID = int.from_bytes(self._readsome(f, 4), "big")
+        self.rate = int.from_bytes(self._readsome(file, 4), "big")
+        self.volume = int.from_bytes(self._readsome(file, 2), "big")
+        self._readsome(file, 10)
+        self.matrix = list(
+            map(lambda x: int.from_bytes(self._readsome(file, 4), "big"), range(9))
+        )
+        self._readsome(file, 24)
+        self.next_track_id = int.from_bytes(self._readsome(file, 4), "big")
 
-    def encode(self):
-        ret = super().encode()
+    def to_bytes(self):
+        ret = super().to_bytes()
         if self.version == 1:
-            ret += self.creationTime.to_bytes(8, byteorder='big')
-            ret += self.modificationTime.to_bytes(8, byteorder='big')
+            ret += self.time[0].to_bytes(8, byteorder='big')
+            ret += self.time[1].to_bytes(8, byteorder='big')
             ret += self.timescale.to_bytes(4, byteorder='big')
             ret += self.duration.to_bytes(8, byteorder='big')
         else:
-            ret += self.creationTime.to_bytes(4, byteorder='big')
-            ret += self.modificationTime.to_bytes(4, byteorder='big')
+            ret += self.time[0].to_bytes(4, byteorder='big')
+            ret += self.time[1].to_bytes(4, byteorder='big')
             ret += self.timescale.to_bytes(4, byteorder='big')
             ret += self.duration.to_bytes(4, byteorder='big')
 
@@ -59,9 +62,11 @@ class Box(FullBox):
         ret += self.volume.to_bytes(2, byteorder='big')
         ret += (0).to_bytes(2, byteorder='big')
         ret += (0).to_bytes(8, byteorder='big')
-        for i in self.matrix:
-            ret += i.to_bytes(4, byteorder='big')
-        for i in range(6):
-            ret += (0).to_bytes(4, byteorder='big')
-        ret += self.nextTrackID.to_bytes(4, byteorder='big')
+        ret += reduce(
+            lambda a, b: a + b, map(lambda x: x.to_bytes(4, byteorder='big'), self.matrix)
+        )
+        ret += reduce(
+            lambda a, b: a + b, map(lambda x: (0).to_bytes(4, byteorder='big'), range(6))
+        )
+        ret += self.next_track_id.to_bytes(4, byteorder='big')
         return ret
